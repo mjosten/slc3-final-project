e63e239cdd1aff9abfe14437ac1e2c5663104e00
mjosten <38707699+mjosten@users.noreply.github.com>
2018-05-26 15:05:34 -0700

    5/26/2018 Continued progress of the calcsim
    _Almost done on the calcsim assembly code which is the simplified version of the book calculator code
    _

--- calcsim.asm
+++ calcsim.asm
@@ -18,31 +18,50 @@
 
 	; Main, get input from user, initialize stackbase
 		LEA R6, STACKBASE
-		LEA R0, PROMPT1	; print prompt 1
+		LEA R0, PROMPT1		; print prompt 1
 		PUTS			
-		GETC		; get user input
+		GETC			; get user input
 		JSR PUSH
+		LEA R0, PROMPT2		; Print Prompt 2
+		PUTS
+		GETC
+		JSR PUSH
+		LEA R0, PROMPT3		; Print Prompt 3
+		PUTS
+		JSR SUM
+		OUT
 		HALT
+
+	; SUM Routine pops 2 from the stack and adds them together and 
+	; returns the sum to R0
+SUM		AND R5, R5, #0
+		ADD R5, R7, #0	; Store R7
+		;pop twice and add to registers
 
 
 	; PUSH routine, R0 onto stack and will check for overflow
-PUSH		RET
+PUSH		ADD R6, R6, #-1		; Decrement stack pointer
+		STR R0, R6, #0		; store in the stack
+		;check overflow, compare stackbase address with current address
+		RET
+		
 
 	; POP routine, load R0 with next number from stack, check for underflow
-POP		RET
+POP		LDR R0, R6, #0		; load from stack
+		ADD R6, R6, #1		; increment stack pointer
+		; check underflow
+		RET
 
-	; Convert to Negative routine, will subtract ZERO value from 
-CONV2NEG	LD R1, NEGZERO
-		; save R7 here
-		JSR POP		; R0 will be loaded with user input char
-		ADD R0, R0, R1	; subtract R0 with negzero to get int value
-
+	
 
 		.BLKW 9
 STACKBASE	.FILL x0000		; stack will be 10 spaces large
+
 PROMPT1		.STRINGZ "Enter the first number to sum: " ; first prompt
 PROMPT2		.FILL x000A		; Second Prompt for user input
 		.STRINGZ "Enter the second number to sum: "
+PROMPT3		.FILL x000A		; Third prompt 
+		.STRINGZ "Sum result" 
 NEGZERO		.FILL x-0030		; negative ascii value of zero
 		.END
 
